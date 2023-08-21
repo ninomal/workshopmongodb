914b454c5acbc8b8092669b263bc53363d22eb5e
nino mal <nino_mrt@hotmail.com>
2023-08-21 20:57:36 -0300

    first commit

---
+++ src/main/java/ninomal/group/mongodb/WorkshopmongodbsApplication.java
@@ -0,0 +1,13 @@
+package ninomal.group.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongodbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongodbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ninomal/group/mongodb/WorkshopmongodbsApplicationTests.java
@@ -0,0 +1,13 @@
+package ninomal.group.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
