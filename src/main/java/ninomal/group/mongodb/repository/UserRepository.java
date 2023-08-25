2fb138900b7e3d15506295c5ef524134e90b7610
nino mal <nino_mrt@hotmail.com>
2023-08-25 18:41:38 -0300

    Operation instance of dataBase

--- src/main/java/ninomal/group/mongodb/repository/UserRepository.java
+++ src/main/java/ninomal/group/mongodb/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import ninomal.group.mongodb.domain.User;
 
+
 public interface UserRepository extends MongoRepository<User, String>{
 	
 
