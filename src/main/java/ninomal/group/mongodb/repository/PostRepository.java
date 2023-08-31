b318a38f5e2e0e671466a7685e3ae93578f0bbec
nino mal <nino_mrt@hotmail.com>
2023-08-31 18:28:36 -0300

    consult with query methods

--- src/main/java/ninomal/group/mongodb/repository/PostRepository.java
+++ src/main/java/ninomal/group/mongodb/repository/PostRepository.java
@@ -3,11 +3,15 @@ package ninomal.group.mongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import ninomal.group.mongodb.domain.Post;
 
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title' : { $regex: '?0', $options: 'i' } }")
+	List<Post> findyByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
--- src/main/java/ninomal/group/mongodb/service/PostService.java
+++ src/main/java/ninomal/group/mongodb/service/PostService.java
@@ -23,7 +23,7 @@ public class PostService {
 		}
 	
 	public List<Post> findByTitle(String text){
-		return post.findByTitleContainingIgnoreCase(text);
+		return post.findyByTitle(text);
 	}
 
 }
